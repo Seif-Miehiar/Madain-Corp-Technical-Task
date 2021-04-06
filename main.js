d41041d67cffc656ecec134ae827090b05379757
Seif-Miehiar <Seif.Miehiar@gmail.com>
2021-04-06 20:36:45 +0300

    fixing infinite loop bug

--- main.js
+++ main.js
@@ -15,12 +15,11 @@ const board_container = document.querySelector(".game-grid");
 const render_board = () => {
 	board_container.innerHTML = "";
 
-	play_board.forEach((e, i) => {
-		console.log(i);
-		board_container.innerHTML += `<div id="block_${i}"
+	play_board.forEach((element, index) => {
+		board_container.innerHTML += `<div id="block_${index}"
       class="block"
-      onclick="addPlayerMove(${i})">
-      ${play_board[i]}
+      onclick="addPlayerMove(${index})">
+      ${play_board[index]}
       </div>`;
 		// if (e == player || e == computer) {
 		// 	document.querySelector(`#block_${i}`).classList.add("occupied");
@@ -35,24 +34,45 @@ const player = "O";
 const computer = "X";
 
 //adding player move.
-const addPlayerMove = (e) => {
-	if (play_board[e] === "") {
+const addPlayerMove = (element) => {
+	if (!board_full && play_board[element] === "") {
 		// assign the value of the player in our Array
-		play_board[e] = player;
+		play_board[element] = player;
 		//re-rendering the board with the updated inputs.
-		render_board();
+		game_loop();
 		//function that will execute the computer's move
 		addComputerMove();
 	}
 };
 
 //function will make a random move as first implementation.
-const addComputerMove = (e) => {
-	do {
-		selected = Math.floor(Math.random() * 9);
-	} while (play_board[selected] !== "");
-	//update the value of that input in our array
-	play_board[selected] = computer;
-	//re-rendering the board with updated values
+const addComputerMove = (element) => {
+	if (!isBoardFull) {
+		do {
+			selected = Math.floor(Math.random() * 9);
+		} while (play_board[selected] !== "");
+		//update the value of that input in our array
+		play_board[selected] = computer;
+		//re-rendering the board with updated values
+		game_loop();
+	}
+};
+
+//fixing the computer moves || infinite looping even if the board is full.
+let isBoardFull = false;
+
+const check_board_complete = () => {
+	let flag = true;
+	play_board.forEach((element) => {
+		if (element !== player && element !== computer) {
+			flag = false;
+		}
+	});
+	isBoardFull = !flag;
+};
+
+//staying dry and invoking one function at once rather than invoking the re-rendering at after every function.
+const game_loop = () => {
 	render_board();
+	check_board_complete();
 };
